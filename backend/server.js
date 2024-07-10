91516db852e59ff1ae6bd2c69175fcfd1271dd29
Fatima Badaoui <fatimina615@gmail.com>
2024-07-10 16:08:16 +0200

    create product router and controller

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
+import productRouter from "./routes/product.router.js";
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to my Shopify API! 🚀",
+  });
+});
+
+app.use("/api/products", productRouter);
+
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on  http://localhost:${PORT}`);
+});
